77f35de3ca7515c2e2a67ffe44a5826c4f89ceff
Rohit Rawat <rohitrawat4073@gmail.com>
2022-08-21 15:44:13 +0530

    Add files via upload

---
+++ Strings.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Strings {
+    public static void main (String args[]){
+       String str1 = new String("Pathi"); //ways to write string
+        String fullName = "Tony Stark";
+       
+       // Scanner sc = new Scanner(System.in);
+       // String name=sc.nextLine();
+        System.out.println(fullName.length());
+
+    
+    }
+}
