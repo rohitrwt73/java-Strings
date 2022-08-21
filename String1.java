26ef7fdf8119e8f0a9802800514897915e0e4f45
Rohit Rawat <rohitrawat4073@gmail.com>
2022-08-21 18:19:58 +0530

    Add files via upload

---
+++ String1.java
@@ -0,0 +1,9 @@
+public class String1 {
+    public static void main(String[] args) {
+        String fullName = "Tony Stark";
+        for(int i=0 ;i <fullName.length(); i++){
+             System.out.println(fullName.charAt(i));//prints character at index i .
+    }
+        }
+       
+}
