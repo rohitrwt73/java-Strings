abb9165e7b5ad075d5e1bfc370312ca63c083906
Rohit Rawat <rohitrawat4073@gmail.com>
2022-08-21 20:06:34 +0530

    Update Strings.java

--- Strings.java
+++ Strings.java
@@ -1,11 +1,12 @@
 import java.util.*;
 public class Strings {
     public static void main (String args[]){
-       String str1 = new String("Pathi"); //ways to write string
-        String fullName = "Tony Stark";
+      String fruits []= {"Mango", "Apple", "Banana"};//ways to write string
+        String str1 = new String("Pathi");           //ways to write string
+        String fullName = "Tony Stark";              //ways to write string
        
-       // Scanner sc = new Scanner(System.in);
-       // String name=sc.nextLine();
+        Scanner sc = new Scanner(System.in);
+        String name=sc.nextLine();
         System.out.println(fullName.length());
 
     
